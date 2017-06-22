fe5097a277839f0e8c5eaf73ed6793335307b189
WashingtonSouza <thewashington.souza@gmail.com>
2017-06-22 10:54:51 -0300

    Criação classe Usuario, instalação e configuração do entity, criação do dataset.

---
+++ LojaComEntity/LojaComEntity/DAOContext.cs
@@ -0,0 +1,21 @@
+﻿using LojaComEntity.DAO;
+using Microsoft.Data.Entity;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaComEntity
+{
+    public class DAOContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["LojaConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(stringConexao);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ LojaComEntity/LojaComEntity/Program.cs
@@ -0,0 +1,32 @@
+﻿using LojaComEntity.DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaComEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DAOContext context = new DAOContext();
+
+            Usuario was = new Usuario()
+            {
+                Nome = "Washington".ToUpper(),
+                Senha = "123".ToUpper()
+            };
+            context.Usuarios.Add(was);
+            context.SaveChanges();
+            context.Dispose();
+
+            Console.WriteLine("salvou o usuário");
+
+            Console.ReadLine();
+        }
+    }
+}
